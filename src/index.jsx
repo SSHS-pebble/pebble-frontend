660226d124e3daa0c711da2c4627fef4bd45f703
Sungbin Jo <pcr910303@icloud.com>
2020-04-01 00:06:48 +0900

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+const React = require("react");
+const ReactDOM = require("react-dom");
+require("typeface-roboto");
+
+const App = require("./components/app.jsx");
+const root = document.getElementById("root");
+
+ReactDOM.render(<App />, root);
